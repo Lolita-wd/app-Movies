df5ad3b8b3f6e7da8d69d120fa9a8309b1796419
Lolita <lolita.ceramist@gmail.com>
2023-06-16 15:16:34 +0300

    Start create app

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict" ;
+
+let numberOffilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+
+let personalMovieDB = {
+        count: numberOffilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+        };
+
+let a = prompt(`Один из последних просмотренных фильмов`, ``),
+    b = prompt(`На сколько оцените его?`, ``),
+    c = prompt(`Один из последних просмотренных фильмов`, ``),
+    d = prompt(`На сколько оцените его?`, ``);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
