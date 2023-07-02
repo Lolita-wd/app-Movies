48dac697f07a0a78ad5ef7183062dc7eb03bd848
Lolita <lolita.ceramist@gmail.com>
2023-07-02 12:16:56 +0300

    Second practical task

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict" ;
 
-let numberOffilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+/*let numberOffilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
 
 let personalMovieDB = {
         count: numberOffilms,
@@ -16,8 +16,45 @@ let a = prompt(`Один из последних просмотренных фи
     d = prompt(`На сколько оцените его?`, ``);
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[c] = d;*/
+
+
+let numberOffilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+
+let personalMovieDB = {
+    count: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+first: for (let i = 0; i < 2 ; i++) {
+           let filmTitle = prompt(`Один из последних просмотренных фильмов`, ``),
+                filmRating = prompt(`На сколько оцените его?`, ``);
+
+           if ((filmTitle && filmRating) == false) {
+               i--;
+               continue first;
+           } else if (filmTitle.length > 50 ) {
+               i--;
+               continue first;
+           };
+
+           personalMovieDB.movies[filmTitle] = filmRating;
+       };
+
+if (personalMovieDB.count < 10) {
+    document.write(`Просмотрено довольно мало фильмов`);
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    document.write(`Вы классический зритель`);
+} else if ( personalMovieDB.count > 30) {
+    document.write(`Вы киноман`);
+} else {
+    document.write(`Произошла ошибка`);
+}
 
 console.log(personalMovieDB);
+
 
 
